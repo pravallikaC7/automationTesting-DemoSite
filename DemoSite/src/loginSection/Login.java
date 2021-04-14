d8d64d0d0b7ede519fbc685c7862863655da838b
pravallikaC7 <pravallikachilukuri7@gmail.com>
2021-04-14 18:08:52 +0530

    Added comments

--- DemoSite/src/loginSection/Login.java
+++ DemoSite/src/loginSection/Login.java
@@ -20,18 +20,28 @@ public class Login {
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(50, TimeUnit.SECONDS);
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);	
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		/*
+		 * Go to http://www.demo.guru99.com/V4/
+		 */
 		driver.get("http://www.demo.guru99.com/V4/");
 		loginSuccessful();
 	}
 	
+	/*
+	 * Enter valid Userid & Password
+	 */
 	public void loginSuccessful() {
+		//Enter valid userID
 		driver.findElement(By.xpath("//input[@name='uid']")).sendKeys("mngr320209");	
+		//Enter valid password
 		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("depehAb");
+		//Click on Login
 		driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
 		String expected = "Manger Id" + " : mng" + "r320209";
 		String actual = driver.findElement(By.cssSelector(".heading3 > td")).getText();
 		Assert.assertEquals(expected, actual);
+		//Verify that Login is successful
 		System.out.println("Login Successful");
 		}
 
