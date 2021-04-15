71374d660fa2285c04c618355cc8667394068278
pravallikaC7 <pravallikachilukuri7@gmail.com>
2021-04-15 20:10:33 +0530

    Created Util File

--- DemoSite/src/loginSection/Login.java
+++ DemoSite/src/loginSection/Login.java
@@ -1,3 +1,8 @@
+/*
+ * Test script for testing Website Guru99 Bank
+ * The test scripts is developed using Selenium Framework
+ *
+ */
 package loginSection;
 
 import java.util.concurrent.TimeUnit;
@@ -12,35 +17,43 @@ import org.junit.Assert;
  */
 public class Login {
 	
-	static WebDriver driver;
+	public static WebDriver driver;
 	
+/*
+ * Setup and launch browser
+ */
 	public void invokeBrowser() {
-		System.setProperty("webdriver.gecko.driver", "D:\\Selenium IDE\\geckodriver-v0.29.1-win64\\geckodriver.exe");
+		//Setup Firefox driver
+		System.setProperty("webdriver.gecko.driver", Util.FIREFOX_PATH);
+		//Launch Firefox 
 		driver = new FirefoxDriver();	
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
-		driver.manage().timeouts().pageLoadTimeout(50, TimeUnit.SECONDS);
+		driver.manage().timeouts().pageLoadTimeout(200, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		/*
-		 * Go to http://www.demo.guru99.com/V4/
-		 */
-		driver.get("http://www.demo.guru99.com/V4/");
 		loginSuccessful();
 	}
 	
 	/*
-	 * Enter valid Userid & Password
+	 * Test Script 01
+       Test Steps
+       1)  Go to http://www.demo.guru99.com/V4/
+       2) Enter valid UserId
+       3) Enter valid Password
+       4) Click Login
+       5) Verify that Login is successful by verifying the Title of Home Page
 	 */
 	public void loginSuccessful() {
+		//Go to Base URL
+		driver.get(Util.BASE_URL);
 		//Enter valid userID
-		driver.findElement(By.xpath("//input[@name='uid']")).sendKeys("mngr320209");	
+		driver.findElement(By.xpath("//input[@name='uid']")).sendKeys(Util.userName);	
 		//Enter valid password
-		driver.findElement(By.xpath("//input[@name='password']")).sendKeys("depehAb");
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(Util.password);
 		//Click on Login
 		driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
-		String expected = "Manger Id" + " : mng" + "r320209";
-		String actual = driver.findElement(By.cssSelector(".heading3 > td")).getText();
-		Assert.assertEquals(expected, actual);
+		String expected = "Guru99 Bank Manager HomePage";
+		Assert.assertEquals(expected, driver.getTitle());
 		//Verify that Login is Successful.
 		System.out.println("Login Successful");
 		}
---
+++ DemoSite/src/loginSection/Util.java
@@ -0,0 +1,14 @@
+package loginSection;
+
+public class Util {
+	
+public final static String FIREFOX_PATH = "D:\\Selenium IDE\\geckodriver-v0.29.1-win64\\geckodriver.exe";
+public final static String BASE_URL = "http://www.demo.guru99.com/V4/";
+public final static String userName = "mngr320209";
+public final static String password = "depehAb";
+
+	public static void main(String[] args) {
+
+	}
+
+}
