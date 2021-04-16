2853662d20a8e92fbe9facef3ea4fdeb27c09eaf
pravallikaC7 <pravallikachilukuri7@gmail.com>
2021-04-17 00:06:11 +0530

    Parameterization

--- DemoSite/src/loginSection/Login.java
+++ DemoSite/src/loginSection/Login.java
@@ -5,33 +5,52 @@
  */
 package loginSection;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.junit.Assert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /*
  * Verify the Login Section of Demo Site 
  */
 public class Login {
 	
-	public static WebDriver driver;
-	
+	public static WebDriver driver;	
 /*
  * Setup and launch browser
  */
 	public void invokeBrowser() {
 		//Setup Firefox driver
-		System.setProperty("webdriver.gecko.driver", Util.FIREFOX_PATH);
+		System.setProperty("webdriver.chrome.driver", Util.FIREFOX_PATH);
 		//Launch Firefox 
-		driver = new FirefoxDriver();	
+		driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(200, TimeUnit.SECONDS);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-		loginSuccessful();
+		getCredentials();
+	}
+	
+	public void getCredentials() {
+		Credentials excelObj = new Credentials();
+		try {
+			 excelObj.readExcel("D:\\Guru99\\Credentials.xlsx","Credentials.xlsx","Login");
+		} catch (IOException e) {
+		}
+		int row = excelObj.loginCredentials.length;
+		for(int i=0; i<row; i++) {
+			loginSuccessful(excelObj.loginCredentials[i][0], excelObj.loginCredentials[i][1]);
+		}
+		
 	}
 	
 	/*
@@ -42,26 +61,54 @@ public class Login {
        3) Enter valid Password
        4) Click Login
        5) Verify that Login is successful by verifying the Title of Home Page
-	 */
-	public void loginSuccessful() {
+	 */    
+	public void loginSuccessful(String userName1, String password1) {
+		String userName = userName1;
+		String password = password1;
 		//Go to Base URL
 		driver.get(Util.BASE_URL);
 		//Enter valid userID
-		driver.findElement(By.xpath("//input[@name='uid']")).sendKeys(Util.userName);	
+		WebElement user = new WebDriverWait(driver, 40).until(ExpectedConditions
+				.elementToBeClickable(By.xpath("//input[@name='uid']")));
+		user.sendKeys(userName);
+		//driver.findElement(By.xpath("//input[@name='uid']")).sendKeys(userName);	
 		//Enter valid password
-		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(Util.password);
+		driver.findElement(By.xpath("//input[@name='password']")).sendKeys(password);
 		//Click on Login
 		driver.findElement(By.xpath("//input[@name='btnLogin']")).click();
-		String expected = "Guru99 Bank Manager HomePage";
-		Assert.assertEquals(expected, driver.getTitle());
-		//Verify that Login is Successful.
-		System.out.println("Login Successful");
+		boolean popupPresence = isAlertPresent();
+		if(popupPresence==false) {
+		String actualTitle = driver.getTitle();
+		if (actualTitle.contains("Guru99 Bank Manager HomePage")) {
+				    System.out.println("Login Successful");
+				    ((JavascriptExecutor) driver).executeScript("window.scrollTo(0,500);");
+				    driver.findElement(By.xpath("//a[@href='Logout.php']")).click();	
+				    driver.switchTo().alert().accept();
+		} 
 		}
+		else {
+					driver.switchTo().alert().accept();
+				    System.out.println("Login Failed");
+		}
+		
+	}
+	
+	public boolean isAlertPresent() 
+	{ 
+	    try 
+	    { 
+	        driver.switchTo().alert(); 
+	        return true; 
+	    }   // try 
+	    catch (NoAlertPresentException Ex) 
+	    { 
+	        return false; 
+	    }   // catch 
+	} 
 
 	public static void main(String[] args) {
 		Login test = new Login();
 		test.invokeBrowser();
-
 	}
 
 }
--- DemoSite/src/loginSection/Util.java
+++ DemoSite/src/loginSection/Util.java
@@ -2,10 +2,9 @@ package loginSection;
 
 public class Util {
 	
-public final static String FIREFOX_PATH = "D:\\Selenium IDE\\geckodriver-v0.29.1-win64\\geckodriver.exe";
+public final static String FIREFOX_PATH = "D:\\Selenium IDE\\chromedriver_win32_v89\\chromedriver.exe";
+//D:\\Selenium IDE\\geckodriver-v0.29.1-win64\\geckodriver.exe
 public final static String BASE_URL = "http://www.demo.guru99.com/V4/";
-public final static String userName = "mngr320209";
-public final static String password = "depehAb";
 
 	public static void main(String[] args) {
 
