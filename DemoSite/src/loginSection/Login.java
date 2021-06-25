68860eee0f4964bd3e3ee3cf50238eda38794841
PravallikaC7 <pravallikachilukuri7@gmail.com>
2021-06-25 17:31:00 +0530

    File name change

--- DemoSite/src/loginSection/Login.java
+++ DemoSite/src/loginSection/Login.java
@@ -86,7 +86,7 @@ public class Login {
 			String expectedId = "Manger Id : "+userName;
 			Assert.assertEquals(actualId, expectedId, "ManagerId verification failed");
 			System.out.println("ManagerId verification successful");
-			Util.screenshotProvider("Login1"+userName);
+			Util.screenshotProvider("Login"+userName);
    }
 }
 	
